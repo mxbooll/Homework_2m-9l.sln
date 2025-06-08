eddd95b2a6b6da556a9b131b0e9a408ff76c4f49
mxbooll <mxbooll@gmail.com>
2025-06-08 10:54:08 +0500

    init

---
+++ Homework_2m-9l/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_2m_9l.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get()
+    {
+        return Ok();
+    }
+}
