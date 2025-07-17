9b65ba8d635a50a6ad7154f5dd0c29d6d6180c05
mxbooll <mxbooll@gmail.com>
2025-07-17 20:39:33 +0500

    добавлены контроллеры

--- Homework_2m-9l/Program.cs
+++ Homework_2m-9l/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddControllers();
 
-app.MapGet("/health/", () => new { status = "OK" });
+var app = builder.Build();
+app.MapControllers();
+
 
 app.Run();
