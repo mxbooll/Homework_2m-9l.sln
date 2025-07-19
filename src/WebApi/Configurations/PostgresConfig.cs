18c1467c70e9fc630db93a79a00da7167511fafd
mxbooll <mxbooll@gmail.com>
2025-07-19 14:13:31 +0500

    доработка приложения

---
+++ src/WebApi/Configurations/PostgresConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework_2m_9l.Configurations;
+
+public class PostgresConfig
+{
+    public string Host { get; set; }
+    public string Port { get; set; }
+    public string Database { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    
+    public string GetConnectionString() =>
+        $"Server={Host}:{Port};" +
+        $"Database={Database};" +
+        $"User Id={Username};" +
+        $"Password={Password};";
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,13 +1,32 @@
+using Homework_2m_9l.Configurations;
 using Homework_2m_9l.Data;
 using Homework_2m_9l.Data.Repository.Users;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<WebServiceDbContext>(opt =>
+builder.Services.Configure<PostgresConfig>(config =>
 {
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgresOtusConnection"));
+    config.Host = builder.Configuration["POSTGRES_HOST"];
+    config.Port = builder.Configuration["POSTGRES_PORT"];
+    config.Database = builder.Configuration["POSTGRES_DB"];
+    config.Username = builder.Configuration["POSTGRES_USER"];
+    config.Password = builder.Configuration["POSTGRES_PASSWORD"];
+});
+
+builder.Services.AddDbContext<WebServiceDbContext>((services, opt) =>
+{
+    if (builder.Environment.IsDevelopment())
+    {
+        opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgresOtusConnection"));
+    }
+    else
+    {
+        var pgConfig = services.GetRequiredService<IOptions<PostgresConfig>>().Value;
+        opt.UseNpgsql(pgConfig.GetConnectionString());
+    }
 });
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
