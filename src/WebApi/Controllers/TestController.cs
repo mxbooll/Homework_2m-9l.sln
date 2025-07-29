100d69c5b059ba6b1a92ea7a6c369441c1d0af33
mxbooll <mxbooll@gmail.com>
2025-07-29 15:32:59 +0500

    доработка приложения

---
+++ src/WebApi/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_2m_9l.Controllers;
+
+[ApiController]
+[Route("api")]
+public class TestController : ControllerBase
+{
+    private readonly Random _random = new(42);
+
+    [HttpGet("a")]
+    public async Task<ActionResult<string>> Method1()
+    {
+        return await DoSomethingAsync("method1", 0, 300);
+    }
+
+    [HttpGet("b")]
+    public async Task<ActionResult<string>> Method2()
+    {
+        return await DoSomethingAsync("method2", 200, 400);
+    }
+    
+    [HttpGet("c")]
+    public async Task<ActionResult<string>> Method3()
+    {
+        var timeout = _random.Next(0, 100);
+        await Task.Delay(timeout);
+        var error = _random.Next(10);
+        throw new Exception($"Error {error}");
+    }
+
+    private async Task<string> DoSomethingAsync(string res, int from, int to)
+    {
+        var timeout = _random.Next(from, to);
+        await Task.Delay(timeout);
+
+        var error = _random.Next(10);
+        if (error > 8)
+            throw new Exception($"Error {error}");
+
+        return $"{res}: {timeout}";
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -3,6 +3,7 @@ using Homework_2m_9l.Data;
 using Homework_2m_9l.Data.Repository.Users;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -32,6 +33,50 @@ builder.Services.AddDbContext<WebServiceDbContext>((services, opt) =>
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
+
+var customHistogram = Metrics.CreateHistogram(
+    "custom_request_duration_seconds",
+    "Custom duration histogram",
+    new HistogramConfiguration
+    {
+        Buckets = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8],
+        LabelNames = ["method", "route", "code"]
+    });
+
+app.Use(async (context, next) =>
+{
+    var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+    try
+    {
+        await next();
+        stopwatch.Stop();
+        
+        customHistogram
+            .WithLabels(context.Request.Method, context.Request.Path, context.Response.StatusCode.ToString())
+            .Observe(stopwatch.Elapsed.TotalSeconds);
+    }
+    catch
+    {
+        stopwatch.Stop();
+        customHistogram
+            .WithLabels(context.Request.Method, context.Request.Path, "500")
+            .Observe(stopwatch.Elapsed.TotalSeconds);
+        throw;
+    }
+});
+
+app.UseHttpMetrics(options =>
+{
+    options.RequestDuration.Enabled = true;
+    options.RequestCount.Enabled = true;
+    options.InProgress.Enabled = true;
+    
+    options.AddCustomLabel("route", context => context.Request.Path);
+});
+
+// app.UseMetricServer(port: 9000); 
+app.MapMetrics();     // эндпоинт /metrics
+
 app.MapControllers();
 
 DbInitializer.InitDb(app);
